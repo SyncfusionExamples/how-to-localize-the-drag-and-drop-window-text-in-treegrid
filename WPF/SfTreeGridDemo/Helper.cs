74fc1d2988afa612f1863d41312110f87be5ef9f
ROOT\amalraj.umapathy <amalraj.umapathy@syncfusion.com>
2018-11-08 16:34:31 +0530

    Sample for localizing the drag and drop window text in treegrid is included.

---
+++ WPF/SfTreeGridDemo/Helper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SfTreeGridDemo
+{
+    public class BaseCommand : ICommand
+    {
+        #region Fields
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+       
+
+        #endregion // Fields
+
+        #region Constructors
+
+        public BaseCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public BaseCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+       
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        #endregion // ICommand Members    
+    }
+}
